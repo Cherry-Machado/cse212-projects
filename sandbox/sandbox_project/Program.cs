cb3288213ef8306d71427ed6476ffa0e8889e1f5
Cherry-Machado <Cherry5467@gmail.com>
2025-07-03 09:45:03 -0300

    Updating

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -2,6 +2,7 @@ using System;
 
 public class Program
 {
+
     static void Main(string[] args)
     {
         // This project is here for you to use as a "Sandbox" to play around
@@ -9,5 +10,23 @@ public class Program
         // one of your projects.
 
         Console.WriteLine("Hello Sandbox World!");
+
+        DoSomething(79);
+
+
+        static void DoSomething(int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                int resi = n % i;
+                if (resi == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                ;
+
+            }
+        }
     }
+
 }
